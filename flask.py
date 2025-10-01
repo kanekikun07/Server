79ddf0bc6ecf0ed5eeeb43f5996de30027a994d0
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2025-10-01 21:08:29 +0530

    Delete flask.py

--- flask.py
+++
@@ -1,7 +0,0 @@
-# Flask example
-from flask import Flask, send_file
-app = Flask(__name__)
-
-@app.route('/download')
-def download_file():
-    return send_file('user_proxies.txt', as_attachment=True)
